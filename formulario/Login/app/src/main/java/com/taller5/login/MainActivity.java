1f595be0ef7d08b6e87ea45703f051c3ace621d1
golden659 <velascocristian309@gmail.com>
2022-10-05 21:15:08 -0500

    consumo de api con boton

--- formulario/Login/app/src/main/java/com/taller5/login/MainActivity.java
+++ formulario/Login/app/src/main/java/com/taller5/login/MainActivity.java
@@ -9,30 +9,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
     Button  enviar;
     TextView forgot;
-    TextView usuario;
-    TextView password;
-    RequestQueue requestQueue;
 
-    private static final String BASE_URL = "http://localhost:8080/consultarAll";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         forgot = (TextView) findViewById(R.id.forgot);
         forgot.setOnClickListener(new View.OnClickListener() {
@@ -47,52 +36,11 @@ public class MainActivity extends AppCompatActivity {
         enviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                 stringRequest();
+                Intent i = new Intent(MainActivity.this, Menu.class);
+                startActivity(i);
             }
         });
 
-
     }
-    private void stringRequest(){
-        usuario = (EditText) findViewById(R.id.editTextTextPersonName);
-        password = (EditText) findViewById(R.id.editTextTextPersonName2);
 
-        JsonArrayRequest request= new JsonArrayRequest(Request.Method.GET, BASE_URL, null,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        int size = response.length();
-                        for(int  i = 0; i< size; i++){
-                            try {
-                                JSONObject jsonObject = new JSONObject(response.get(i).toString());
-                                String email = jsonObject.getString("email");
-
-                                if(usuario.equals(email)){
-
-                                enviar = (Button) findViewById(R.id.submit);
-                                enviar.setOnClickListener(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View view) {
-                                        Intent i = new Intent(MainActivity.this, Menu.class);
-                                        startActivity(i);
-                                    }
-                                });
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-
-                        }
-
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                });
-// Add the request to the RequestQueue.
-        requestQueue.add(request);
-    }
 }
--- formulario/Login/app/src/main/java/com/taller5/login/Menu.java
+++ formulario/Login/app/src/main/java/com/taller5/login/Menu.java
@@ -3,12 +3,51 @@ package com.taller5.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class Menu extends AppCompatActivity {
+    TextView menu;
+    RequestQueue requestQueue;
+    private static final String BASE_URL = "http://localhost:8080/consultarAll";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        requestQueue = Volley.newRequestQueue(this);
+        //UI
+        initUi();
+        //Rest
+        stringRequest();
+    }
+
+    private void initUi(){
+        menu = findViewById(R.id.menu);
+    }
+
+    private void stringRequest(){
+        StringRequest request = new StringRequest(
+                Request.Method.GET, BASE_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                    menu.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }
+        );
+        requestQueue.add(request);
     }
 }
